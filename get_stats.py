445f4cd2e2f627856d385927b61cc8d3a74411da
Pavel Krotkov <pavel.krotkov@gmail.com>
2024-08-20 11:14:44 -0400

    import missing

--- get_stats.py
+++ get_stats.py
@@ -5,6 +5,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
+from selenium.common.exceptions import TimeoutException, StaleElementReferenceException
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 import os
 import time
